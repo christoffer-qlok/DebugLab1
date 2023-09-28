2818ce661b7a7fa620d0ac7b574cb86de3e837fe
christoffer.fjellstrom <christoffer.fjellstrom@qlok.se>
2023-09-28 07:14:10 +0200

    Add project files.

---
+++ DebugLab1/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace DebugLab1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ReverseString("Hello world!"));
+        }
+
+
+        static string ReverseString(string str)
+        {
+            char[] chars = str.ToCharArray();
+            char[] answer = new char[chars.Length];
+
+            for (int i = 0; i < chars.Length; i++)
+            {
+                answer[i] = chars[chars.Length - i];
+            }
+            return new string(answer);
+        }
+    }
+}
